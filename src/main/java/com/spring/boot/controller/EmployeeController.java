ae3288dc2d3ae83b548947a395a9fd94776e69ab
Micael Jerry <hei.jerry.2@gmail.com>
2023-07-02 16:18:22 +0300

    chore: update controller

--- src/main/java/com/spring/boot/controller/EmployeeController.java
+++ src/main/java/com/spring/boot/controller/EmployeeController.java
@@ -16,17 +16,15 @@ import java.util.List;
 @Controller
 @AllArgsConstructor
 public class EmployeeController {
-    @GetMapping(path = "/employees/post")
-    public String anInt(HttpSession session) throws JsonProcessingException {
-        List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(null, "a", "fidimalala", new Date()));
-        employees.add(new Employee(null, "c", "fidimalala", new Date()));
-        session.setAttribute("employees",Convert.employeeListAsString(employees));
-        return "employees";
-    }
 
     @GetMapping(path = "/employees")
     public String getEmployees(HttpSession session, Model model) throws JsonProcessingException {
+        if (session.getAttribute("employees") == null) {
+            List<Employee> newEmployees = new ArrayList<>();
+            newEmployees.add(new Employee(null, "a", "fidimalala", new Date()));
+            newEmployees.add(new Employee(null, "c", "fidimalala", new Date()));
+            session.setAttribute("employees",Convert.employeeListAsString(newEmployees));
+        }
         List<Employee> employees = Convert.stringAsEmployeeList((String) session.getAttribute("employees"));
         model.addAttribute("employees",employees);
         return "employees";
