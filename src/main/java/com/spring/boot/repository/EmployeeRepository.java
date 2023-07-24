cd38eb62402692c96de4109e0d7f434b4a00b645
Micael Jerry <hei.jerry.2@gmail.com>
2023-07-24 08:33:51 +0300

    chore: add jpql request

--- src/main/java/com/spring/boot/repository/EmployeeRepository.java
+++ src/main/java/com/spring/boot/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import com.spring.boot.model.Employee;
 import com.spring.boot.model.Sex;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +12,25 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     Employee getByCnapsNumber(String cnapsNumber);
-    List<Employee> findAllByFunctionContainsIgnoreCaseAndLastnameContainsIgnoreCaseAndFirstnameContainsIgnoreCase(String function, String lastName, String firstName, Sort sort);
+
+//    @Query("SELECT e FROM Employee e WHERE LOWER(e.function) LIKE %:function% AND LOWER(e.lastname) LIKE %:lastname% AND LOWER(e.firstname) LIKE %:firstname%")
+//    List<Employee> findAllByFunctionContainsIgnoreCaseAndLastnameContainsIgnoreCaseAndFirstnameContainsIgnoreCase(
+//            String function,
+//            String lastname,
+//            String firstname,
+//            Sort sort
+//    );
+
+    List<Employee> findAllByFunctionContainsIgnoreCaseAndLastnameContainsIgnoreCaseAndFirstnameContainsIgnoreCase(String function, String lastname, String firstname, Sort sort);
+
+//    @Query("SELECT e FROM Employee e WHERE LOWER(e.function) LIKE %:function% AND LOWER(e.lastname) LIKE %:lastname% AND LOWER(e.firstname) LIKE %:firstname% AND e.sex = :sex")
+//    List<Employee> findAllByFunctionContainsIgnoreCaseAndLastnameContainsIgnoreCaseAndFirstnameContainsIgnoreCaseAndSex(
+//            String function,
+//            String lastname,
+//            String firstname,
+//            Sex sex,
+//            Sort sort
+//    );
 
     List<Employee> findAllByFunctionContainsIgnoreCaseAndLastnameContainsIgnoreCaseAndFirstnameContainsIgnoreCaseAndSex(String function, String lastname, String firstname, Sex sex, Sort sort);
 }
