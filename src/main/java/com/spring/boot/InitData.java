d6aaf29a07768d86e419f253bd84cce1b337dacf
Micael Jerry <hei.jerry.2@gmail.com>
2023-08-21 15:23:11 +0300

    chore: update init data, clean pre destroy

--- src/main/java/com/spring/boot/InitData.java
+++ src/main/java/com/spring/boot/InitData.java
@@ -1,8 +1,10 @@
 package com.spring.boot;
 
 import com.spring.boot.employee.model.User;
+import com.spring.boot.employee.repository.UserRepository;
 import com.spring.boot.employee.service.UserService;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +12,16 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class InitData {
     private UserService userService;
+    private UserRepository userRepository;
 
     @PostConstruct
     public void createUser() {
         User user = new User(null, "admin", "admin", null);
         userService.save(user);
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        userRepository.deleteAll();
     }
 }
